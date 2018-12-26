c388fb8abf516fb4e5ddb96aa2f963deda3235ee
Kasper Socha <kasper.socha@jayway.com>
2018-12-26 11:55:47 +0100

    Fix snake movement

--- snake/src/Main.elm
+++ snake/src/Main.elm
@@ -15,6 +15,24 @@ grid =
     Matrix.repeat 20 20 0
 
 
+type alias Position =
+    { x : Int, y : Int }
+
+
+type Direction
+    = Up
+    | Down
+    | Left
+    | Right
+
+
+type alias Snake =
+    { head : Position
+    , tail : List Position
+    , direction : Direction
+    }
+
+
 type alias Model =
     { snake : Snake
     }
@@ -23,11 +41,19 @@ type alias Model =
 type Msg
     = Tick Time.Posix
     | KeyPress Direction
+    | Noop
 
 
 init : () -> ( Model, Cmd msg )
 init _ =
-    ( { snake = { position = [ Position 10 10, Position 10 11 ], direction = Down } }, Cmd.none )
+    ( { snake =
+            { head = Position 10 10
+            , tail = [ Position 10 11 ]
+            , direction = Down
+            }
+      }
+    , Cmd.none
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -38,6 +64,9 @@ update msg model =
 
         KeyPress direction ->
             ( { model | snake = updateSnakeDirection model.snake direction }, Cmd.none )
+
+        Noop ->
+            ( model, Cmd.none )
 
 
 wrap : Int -> Int
@@ -52,34 +81,74 @@ wrap num =
         num
 
 
+isValidDirectionChange : Direction -> Direction -> Bool
+isValidDirectionChange old new =
+    case new of
+        Up ->
+            old /= Down
+
+        Down ->
+            old /= Up
+
+        Left ->
+            old /= Right
+
+        Right ->
+            old /= Left
+
+
 updateSnakeDirection : Snake -> Direction -> Snake
-updateSnakeDirection snake direction =
-    case direction of
-        Unknown ->
+updateSnakeDirection snake newDirection =
+    let
+        { direction } =
             snake
 
-        _ ->
-            { snake | direction = direction }
+        isValidMove =
+            isValidDirectionChange direction newDirection
+    in
+    if isValidMove then
+        { snake | direction = newDirection }
+
+    else
+        snake
 
 
 updateSnakePosition : Snake -> Snake
 updateSnakePosition snake =
-    -- TODO: Given [Tail, Body..., Head] drop Tail NewTailAndBody = Body + Head new Head = according to direction
-    case snake.direction of
+    let
+        { head, tail, direction } =
+            snake
+
+        tailLength =
+            List.length tail
+
+        newTail =
+            head :: List.take (tailLength - 1) tail
+
+        newHead =
+            updateSnakeHead direction head
+    in
+    { snake | head = newHead, tail = newTail }
+
+
+updateSnakeHead : Direction -> Position -> Position
+updateSnakeHead direction position =
+    let
+        { x, y } =
+            position
+    in
+    case direction of
         Up ->
-            { snake | position = List.map (\p -> { p | y = wrap (p.y - 1) }) snake.position }
+            { position | y = wrap (y - 1) }
 
         Down ->
-            { snake | position = List.map (\p -> { p | y = wrap (p.y + 1) }) snake.position }
-
-        Right ->
-            { snake | position = List.map (\p -> { p | x = wrap (p.x + 1) }) snake.position }
+            { position | y = wrap (y + 1) }
 
         Left ->
-            { snake | position = List.map (\p -> { p | x = wrap (p.x - 1) }) snake.position }
+            { position | x = wrap (x - 1) }
 
-        Unknown ->
-            snake
+        Right ->
+            { position | x = wrap (x + 1) }
 
 
 subscriptions : Model -> Sub Msg
@@ -111,54 +180,57 @@ toDirection string =
             KeyPress Down
 
         _ ->
-            KeyPress Unknown
+            Noop
 
 
-viewGrid : List Position -> Int -> Int -> Int -> Html Msg
-viewGrid snakePositions row col content =
+viewGrid : Int -> Int -> Int -> Html Msg
+viewGrid row col content =
+    viewRect "white" row col
+
+
+viewSnake : Snake -> List (Html Msg)
+viewSnake { head, tail } =
     let
-        hasSnake =
-            List.any (\p -> p.x == row && p.y == col) snakePositions
-
-        color =
-            if hasSnake then
-                "black"
-
-            else
-                "white"
+        positions =
+            head :: tail
     in
-    rect [ x (String.fromInt (row * 20)), y (String.fromInt (col * 20)), width "20", height "20", stroke "black", fill color ] []
+    List.map (\p -> viewRect "black" p.x p.y) positions
 
 
-type alias Position =
-    { x : Int, y : Int }
-
-
-type Direction
-    = Up
-    | Down
-    | Left
-    | Right
-    | Unknown
-
-
-type alias Snake =
-    { position : List Position
-    , direction : Direction
-    }
+viewRect : String -> Int -> Int -> Html Msg
+viewRect fillColor row col =
+    rect
+        [ x (String.fromInt (row * 20))
+        , y (String.fromInt (col * 20))
+        , width "20"
+        , height "20"
+        , stroke "black"
+        , fill fillColor
+        ]
+        []
 
 
 view : Model -> Html Msg
 view model =
     svg
         [ width "800", height "800", viewBox "0 0 800 800" ]
-        (Matrix.indexedMap
-            (viewGrid model.snake.position)
-            grid
-            |> Matrix.toArray
-            |> Array.toList
-        )
+    <|
+        List.append
+            (Matrix.indexedMap
+                viewGrid
+                grid
+                |> Matrix.toArray
+                |> Array.toList
+            )
+            (viewSnake
+                model.snake
+            )
 
 
 main =
-    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
