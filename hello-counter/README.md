17a1f6e751f4fe1d596891e5ddbfa90db84860c8
Filip Bontin <filip.bontin@jayway.com>
2019-04-26 09:16:30 +0200

    add additional readme

--- hello-counter/README.md
+++ hello-counter/README.md
@@ -1,3 +1,8 @@
 # "Hello counter!" in Elm
 
 Navigate to this folder and run `elm reactor`
+
+## Continuations
+
+* Add a `reset` button which sets the state to zero
+* Never let the counter get negative
--- hello-counter/src/Main.elm
+++ hello-counter/src/Main.elm
@@ -5,15 +5,16 @@ import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
 
-main =
-    Browser.sandbox { init = 0, update = update, view = view }
-
-
 type Msg
     = Increment
     | Decrement
 
 
+type alias Model =
+    Int
+
+
+update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
@@ -23,9 +24,15 @@ update msg model =
             model - 1
 
 
+view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Decrement ] [ text "-" ]
         , div [] [ text (String.fromInt model) ]
         , button [ onClick Increment ] [ text "+" ]
         ]
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox { init = 0, update = update, view = view }
